c2e350bbd67686f57f572bf46d9425989793899c
falak50 <83400991+falak50@users.noreply.github.com>
2023-09-30 08:28:36 -1200

    server create

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ cors = require('cors');
 const port = process.env.PORT || 5000;
 
 const categories = require('./data/categories.json');
+const news= require('./data/news.json');
 
 app.use(cors());
 
@@ -13,6 +14,34 @@ app.get('/',(req,res ) => {
 app.get('/categories',(req,res)=>{
     res.send(categories);
 })
+
+app.get('/news',(req,res) => {
+    res.send(news);
+})
+app.get('/news/:id',(req,res) => {
+     const id = req.params.id;
+     const selectedNews = news.find(n => n._id === id);//only olne so find
+     res.send(selectedNews);
+})
+app.get('/categories/:id',(req,res)=>{
+ const id = parseInt(req.params.id);
+ console.log(id);
+ if(id===0){
+    res.send(news);
+ }else {
+    const categoryNews = news.filter(n=> parseInt(n.category_id) === id);
+    res.send(categoryNews);
+ }
+ // you can check == then no pb string or num
+//  if(id==0){
+//     res.send(news);
+//  }else {
+//     const categoryNews = news.filter(n=>n.category_id === id);
+//     res.send(categoryNews);
+//  }
+})
+
+
 
 app.listen(port,() => {
     console.log(`Drogon API is running on port :${port}`)
